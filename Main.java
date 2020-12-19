a459734207a44c80a469c244d715b89186bca9aa
eniso-dev <76095283+eniso-dev@users.noreply.github.com>
2020-12-19 11:20:59 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("BONJOUR LA VIE!");
+        System.out.println("Djerba By Night!");
         System.out.println("Bonne chance pour les exams");
     }
 }
