9c81f54fc58b58cff5d9e8ef7aa606bb5c738bba
eniso-dev <76095283+eniso-dev@users.noreply.github.com>
2021-01-08 16:39:40 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("sOUSSA By Night!");
-        System.out.println("Bonne chance pour les PFE");
+        System.out.println("Bonjour La vie!");
+  
     }
 }
 
